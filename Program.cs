d5452450d3ca38d2d9f4b4b7bf46193a2edafd73
Janos <kelemen.janos@gmail.com>
2020-04-10 20:49:29 +0100

    Basic Linq VS Deforest Aggregate VS Deforest Sum

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Deforesting.StopwatchHelper;
+
+namespace Deforesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new int[100000];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i;
+            }
+
+            StopwatchAction(() => QueryWithWhereSelectSum(data));
+            StopwatchAction(() => DeforestWithAggregate(data));
+            StopwatchAction(() => DeforestWithSingleSum(data));
+        }
+
+        static void QueryWithWhereSelectSum(IEnumerable<int> list)
+        {
+            long sum = list.AsParallel()
+                .Where(i => i % 2 == 0)
+                .Select(i => i + i)
+                .Sum(i => (long)i);
+
+            Console.WriteLine($"QueryWithWhereSelectSum: {sum}");
+        }
+
+        static void DeforestWithAggregate(IEnumerable<int> list)
+        {
+            long sum = list.AsParallel()
+                .Aggregate(0L, (aggr, i) => i % 2 == 0 ? aggr + i + i : aggr);
+
+            Console.WriteLine($"DeforestingWithAggregate: {sum}");
+        }
+
+        static void DeforestWithSingleSum(IEnumerable<int> list)
+        {
+            long sum = list.AsParallel()
+                .Sum(i => (long)i % 2 == 0 ? (long)i + i : 0L);
+
+            Console.WriteLine($"DeforestingWithSingleSum: {sum}");
+        }
+    }
+}
---
+++ StopwatchHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Deforesting
+{
+    public static class StopwatchHelper
+    {
+        public static void StopwatchAction(Action action)
+        {
+            // Stopwatch to determine the execution time for an application
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            action();
+
+            // Stop measuring time
+            stopwatch.Stop();
+
+            // Format and display the TimeSpan value.
+            var ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
